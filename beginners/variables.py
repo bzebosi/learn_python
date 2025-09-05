22bf5ece8ad01bed73404d39984c2e7ec70b4a03
bzebosi <zebosibrian@gmail.com>
2025-09-04 23:35:32 -0700

    first python script

---
+++ beginners/variables.py
@@ -0,0 +1,20 @@
+# How to handle strings i.e. print
+
+# Ask User for their name 
+name = input("what's your name? ")
+
+# say hello to user
+
+# version 1, + means join
+print("hello, " + name)
+
+# version 2
+print("hello,", name)
+
+# version 3 includes sep = ' ' space and end="nothing"
+# sep and end are name parameters
+print("hello,", name, sep=' ', end="\n")
+
+# introduction parameters
+
+print("hello, \"friend\"")
